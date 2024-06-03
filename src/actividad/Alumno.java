8008ccee2045049060d0049840571df120ee3e10
Rosa <rosa@gmamicom>
2024-06-03 18:04:31 -0300

    Java

---
+++ src/actividad/Alumno.java
@@ -0,0 +1,27 @@
+package actividad;
+
+public class Alumno {
+	
+	String nombre;
+	String apellido;
+	String dni;
+	int fnacimiento;
+	String direccion;
+	String telefono;
+	
+//construcor
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public  String getNombre() {
+		return nombre;
+	}
+	
+	public String setNombre() {
+		return nombre;
+	}
+	
+
+}
---
+++ src/actividad/Escuela.java
@@ -0,0 +1,12 @@
+package actividad;
+
+public class Escuela {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Alumno joaquin=new Alumno();
+
+	}
+
+}
